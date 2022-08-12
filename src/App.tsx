5a190d85885894cad45706168a8782e7f1d52c27
뱀 <54323618+viyamKUN@users.noreply.github.com>
2022-08-13 00:55:07 +0900

    Add board

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+interface SquareProps {
+  value: number
+}
+
+function Square(props:SquareProps) {
+  return (
+    <button className='square'>
+      {props.value}
+    </button>
+  );
+}
+
+function Board() {
+  const renderSquare = (i:number) : React.ReactNode => {
+    return (
+      <Square
+        value={i}
+      />
+    );
+  }
+
+  const status = "Next: X";
+  return (
+    <div>
+      <div className='status'>{status}</div>
+      <div>
+          <div className="board-row">
+              {renderSquare(0)}
+              {renderSquare(1)}
+              {renderSquare(2)}
+          </div>
+          <div className="board-row">
+              {renderSquare(3)}
+              {renderSquare(4)}
+              {renderSquare(5)}
+          </div>
+          <div className="board-row">
+              {renderSquare(6)}
+              {renderSquare(7)}
+              {renderSquare(8)}
+          </div>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='game'>
+      <div className='game-board'>
+        <Board />
+      </div>
+      <div className='game-info'>
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
     </div>
   );
 }
