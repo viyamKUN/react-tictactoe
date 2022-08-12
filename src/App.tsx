39c33a2f132202272aabe2816bf8765728d03148
뱀 <54323618+viyamKUN@users.noreply.github.com>
2022-08-13 02:37:16 +0900

    Add click event

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-interface SquareProps {
-  value: number
+type SquareProps = {
+  onClick(): void
+};
+
+type SquareState = {
+  value: string;
+};
+
+class Square extends React.Component<SquareProps, SquareState> {
+  state: SquareState = {
+    value: "",
+  };
+
+  render() {
+    return (
+      <button className='square' onClick={() => {
+        this.props.onClick()
+        this.setState(state => ({value: "X"}))
+      }}>
+        {this.state.value}
+      </button>
+    );
+  }
 }
 
-function Square(props:SquareProps) {
-  return (
-    <button className='square'>
-      {props.value}
-    </button>
-  );
+interface BoardProps {
+  onClick(i: number): void
 }
 
-function Board() {
-  const renderSquare = (i:number) : React.ReactNode => {
+function Board(props: BoardProps) {
+  const renderSquare = (i: number): React.ReactNode => {
     return (
       <Square
-        value={i}
+        onClick={() => props.onClick(i)}
       />
     );
   }
@@ -27,31 +44,42 @@ function Board() {
     <div>
       <div className='status'>{status}</div>
       <div>
-          <div className="board-row">
-              {renderSquare(0)}
-              {renderSquare(1)}
-              {renderSquare(2)}
-          </div>
-          <div className="board-row">
-              {renderSquare(3)}
-              {renderSquare(4)}
-              {renderSquare(5)}
-          </div>
-          <div className="board-row">
-              {renderSquare(6)}
-              {renderSquare(7)}
-              {renderSquare(8)}
-          </div>
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
       </div>
     </div>
   );
 }
 
+function UpdateData(updatedIndex: number) {
+  gamedata[updatedIndex] = turn;
+  turn = turn % 2 + 1;
+  console.log(gamedata)
+}
+
+var turn = 1;
+const gamedata = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
 function App() {
   return (
     <div className='game'>
       <div className='game-board'>
-        <Board />
+        <Board
+          onClick={(i: number) => { UpdateData(i) }}
+        />
       </div>
       <div className='game-info'>
         <div>{/* status */}</div>
